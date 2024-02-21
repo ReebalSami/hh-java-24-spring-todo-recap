9ece2dc3299f6c4f1b4a70066cbc89c8c3ebab5b
Reebal Sami <reebal.sami@gmail.com>
2024-02-21 14:46:04 +0100

    Initial commit

---
+++ src/main/java/neuefisched/de/hhjava24springtodorecap/HhJava24SpringTodoRecapApplication.java
@@ -0,0 +1,13 @@
+package neuefisched.de.hhjava24springtodorecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhJava24SpringTodoRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhJava24SpringTodoRecapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neuefisched/de/hhjava24springtodorecap/HhJava24SpringTodoRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package neuefisched.de.hhjava24springtodorecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhJava24SpringTodoRecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
